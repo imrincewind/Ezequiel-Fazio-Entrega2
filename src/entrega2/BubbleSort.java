49c13c5b11dc1225fe4a662698f5ac65e4f08c86
imrincewind <ezfazio@gmail.com>
2017-04-05 18:01:47 -0300

    first commit

---
+++ src/entrega2/BubbleSort.java
@@ -0,0 +1,18 @@
+package entrega2;
+
+public class BubbleSort {
+	
+	
+	
+	public static void burbuja(int [] A){
+        int i, j, aux;
+        for(i=0;i<A.length-1;i++)
+             for(j=0;j<A.length-i-1;j++)
+                  if(A[j+1]<A[j]){
+                     aux=A[j+1];
+                     A[j+1]=A[j];
+                     A[j]=aux;
+                  }
+}
+
+}
---
+++ src/entrega2/Main.java
@@ -0,0 +1,65 @@
+package entrega2;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Main {
+	/*
+	 * Para poder observar una diferencia entre ambos metodos de ordenamiento
+	 * fue necesario utilizar arreglos de gran tamaño. Con estos arreglos y al
+	 * observar los resultados de tiempo, se hace notoria la diferencia entre
+	 * ambos metodos, siendo mucho más eficiente el MERGESORT
+	 *
+	 */
+
+	public static void main(String[] args) {
+		final int SIZE = 30000000;
+		int[] arregloB = new int[SIZE];
+		int[] arregloM = new int[SIZE];
+		BubbleSort bubb = new BubbleSort();
+
+		for (int i = 0; i < SIZE; i++) {
+			int temp = (int) (Math.random() * SIZE);
+			arregloB[i] = temp;
+			arregloM[i] = temp;
+		}
+
+		System.out.println();
+		System.out.println("ARREGLO ORDENADO BURBUJA");
+		Date inicioArray = new Date();
+		bubb.burbuja(arregloB);
+		Date finalArray = new Date();
+
+		// ------------------------ EJERCICIO 4 ----------------------
+		MergeSort merge = new MergeSort();
+		Date inicioMerge = new Date();
+		merge.sort(arregloM);
+		Date finalMerge = new Date();
+
+		System.out.println();
+		System.out.println(inicioArray);
+		System.out.println(finalArray);
+
+		long resultadoArray = finalArray.getTime() - inicioArray.getTime();
+		System.out.println(resultadoArray / 1000 + " segundos BURBUJA");
+
+		System.out.println();
+		System.out.println(inicioMerge);
+		System.out.println(finalMerge);
+
+		long resultadoMerge = finalMerge.getTime() - inicioMerge.getTime();
+		System.out.println(resultadoMerge / 1000 + " segundos MERGESORTE");
+
+		// SE IMPRIMEN LOS ÚLTIMOS 10000 PARA OBSERBAR QUE HAYA FUNCIONADO EL
+		// ORDENAMIENTO EN AMBOS CASOS
+		for (int i = SIZE - 1; i > SIZE - 10000; i--) {
+			System.out.print(arregloB[i] + " - ");
+		}
+		System.out.println("");
+		for (int i = SIZE - 1; i > SIZE - 10000; i--) {
+			System.out.print(arregloM[i] + " - ");
+		}
+
+	}
+}
